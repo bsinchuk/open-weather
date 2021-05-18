db3cad0db98dd02395db967fa9715168aa563dee
bsinchuk <bogdancse@gmail.com>
2021-05-18 16:23:58 +0300

    Building basic layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import { Grid, CssBaseline } from '@material-ui/core';
 import Adder from './components/Adder';
-import './App.css';
+import Dashboard from './components/Dashboard';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  'spacing-xs-3': {
+    width: '100%',
+    margin: 0,
+  },
+  'item-wrapper': {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItem: 'center',
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <React.Fragment>
       <CssBaseline />
-      <Grid container justify="center" >
-        <Adder />
+      <Grid
+        container
+        spacing={3}
+        direction="column"
+        justify="flex-start"
+        alignItems="center"
+        className={classes['spacing-xs-3']} >
+        <Grid item className={classes['item-wrapper']}>
+          <Adder />
+        </Grid>
+        <Grid item>
+          <Dashboard />
+        </Grid>
       </Grid>
     </React.Fragment>
   );
